13a4c93fd36022e47dc0242430946eddbc4145ce
alicalimli <alicalimli76@gmail.com>
2022-06-12 11:48:54 +0300

    created first route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Start from "./Components/Start";
 import { StrictMode } from "react";
 
 const App = () => {
   return (
     <StrictMode>
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Start />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };
