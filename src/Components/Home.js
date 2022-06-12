5e4d6a4d3387683af6be067e3952b4fe6ededb4c
alicalimli <alicalimli76@gmail.com>
2022-06-12 11:39:31 +0300

    stlyed homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
     <div className="start-container">
       <h1>Quizzical</h1>
       <p>Answer fun trivia questions! </p>
-      <button>Start quiz</button>
+      <button className="start-btn">Start quiz</button>
     </div>
   );
 };
