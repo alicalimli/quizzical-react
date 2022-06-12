5188bacb593293a6294c07ca2fe169501014f59f
alicalimli <alicalimli76@gmail.com>
2022-06-12 11:33:12 +0300

    stylings

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,11 @@
 const Home = () => {
   return (
-    <div>
+    <div className="start-container">
       <h1>Quizzical</h1>
       <p>Answer fun trivia questions! </p>
       <button>Start quiz</button>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
