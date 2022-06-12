d76e5de8078f4b4dc2bcd5ba521173900da33983
alicalimli <alicalimli76@gmail.com>
2022-06-12 11:19:35 +0300

    added Home component

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Home from './Components'
+
+ReactDOM.render(<Home />, document.getElementById("root"))
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Quizzical</h1>
+      <p>Answer fun trivia questions! </p>
+      <button>Start quiz</button>
+    </div>
+  );
+}
+
+export default Home
