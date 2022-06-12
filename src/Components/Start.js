04aae96ff0e570506d73d70eaac4bbe9da829009
alicalimli <alicalimli76@gmail.com>
2022-06-12 11:49:59 +0300

    used Link from router

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Start = () => {
   return (
     <div className="start-container">
       <h1>Quizzical</h1>
       <p>Answer fun trivia questions! </p>
-      <button className="start-btn">Start quiz</button>
+      <Link to="/" className="btn start-btn">
+        Start quiz
+      </Link>
     </div>
   );
 };
