5ac6b832e876a112e4cb75a00d57dbbb1ec8833a
alicalimli <alicalimli76@gmail.com>
2022-06-12 12:19:22 +0300

    style updates and responsive design

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -31,6 +31,96 @@ const Quiz = () => {
           </li>
         </ul>
       </div>
+      <div className="quiz-container">
+        <h1 className="quiz-question">
+          How many cores does the Intel i7-6950X have?
+        </h1>
+        <ul className="quiz-answers">
+          <li className="quiz-answer">
+            <button
+              data-answer-type="correct"
+              className="active quiz-answer-btn"
+            >
+              10
+            </button>
+          </li>
+          <li className="quiz-answer">
+            <button data-answer-type="incorrect" className="quiz-answer-btn">
+              12
+            </button>
+          </li>
+          <li className="quiz-answer">
+            <button data-answer-type="incorrect" className="quiz-answer-btn">
+              18
+            </button>
+          </li>
+          <li className="quiz-answer">
+            <button data-answer-type="incorrect" className="quiz-answer-btn">
+              4
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="quiz-container">
+        <h1 className="quiz-question">
+          How many cores does the Intel i7-6950X have?
+        </h1>
+        <ul className="quiz-answers">
+          <li className="quiz-answer">
+            <button
+              data-answer-type="correct"
+              className="active quiz-answer-btn"
+            >
+              10
+            </button>
+          </li>
+          <li className="quiz-answer">
+            <button data-answer-type="incorrect" className="quiz-answer-btn">
+              12
+            </button>
+          </li>
+          <li className="quiz-answer">
+            <button data-answer-type="incorrect" className="quiz-answer-btn">
+              18
+            </button>
+          </li>
+          <li className="quiz-answer">
+            <button data-answer-type="incorrect" className="quiz-answer-btn">
+              4
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="quiz-container">
+        <h1 className="quiz-question">
+          How many cores does the Intel i7-6950X have?
+        </h1>
+        <ul className="quiz-answers">
+          <li className="quiz-answer">
+            <button
+              data-answer-type="correct"
+              className="active quiz-answer-btn"
+            >
+              10
+            </button>
+          </li>
+          <li className="quiz-answer">
+            <button data-answer-type="incorrect" className="quiz-answer-btn">
+              12
+            </button>
+          </li>
+          <li className="quiz-answer">
+            <button data-answer-type="incorrect" className="quiz-answer-btn">
+              18
+            </button>
+          </li>
+          <li className="quiz-answer">
+            <button data-answer-type="incorrect" className="quiz-answer-btn">
+              4
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
