33e5cea7cfa40460ad3e3542a82d197395f9c430
alicalimli <alicalimli76@gmail.com>
2022-06-14 09:59:42 +0300

    seperated the form as a component from start.js

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -1,18 +1,10 @@
-import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import StartForm from "./StartForm";
 
 const Start = () => {
-  const navigate = useNavigate();
-  const [difficulty, setDifficulty] = useState("easy");
   const [isPending, setIsPending] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
   const [categories, setCategories] = useState([]);
-  const [category, setCategory] = useState();
-
-  const formActionHandler = (e) => {
-    e.preventDefault();
-    navigate(`/quiz/${difficulty}/${category}`);
-  };
 
   useEffect(() => {
     fetcCategoryData();
@@ -38,55 +30,7 @@ const Start = () => {
     <div className="start-page">
       {isPending && <h1>loading..</h1>}
       {errorMsg && <h1>{errorMsg}</h1>}
-      {categories.length !== 0 ? (
-        <div className="start-container">
-          {" "}
-          <h1>Quizzical</h1>
-          <p>Answer fun trivia questions! </p>
-          <form className="quiz-form" onSubmit={formActionHandler}>
-            <label htmlFor="select-category">
-              Category
-              <select
-                required
-                id="select-category"
-                onChange={(e) => setCategory(e.target.value)}
-                onBlur={(e) => setCategory(e.target.value)}
-              >
-                <option
-                  value=""
-                  style={{ display: "none" }}
-                  default
-                  selected
-                  disabled
-                >
-                  Please select a category
-                </option>
-                {categories.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                ))}
-              </select>
-            </label>
-            <label htmlFor="select-difficulty">
-              Difficulty
-              <select
-                id="select-difficulty"
-                onChange={(e) => setDifficulty(e.target.value)}
-                onBlur={(e) => setDifficulty(e.target.value)}
-              >
-                <option value="easy">Easy</option>
-                <option value="medium">Medium</option>
-                <option value="hard">Hard</option>
-              </select>
-            </label>
-
-            <button className="btn start-btn">Start quiz</button>
-          </form>
-        </div>
-      ) : (
-        ""
-      )}
+      {categories.length && <StartForm categories={categories} />}
     </div>
   );
 };
---
+++ src/Components/StartForm.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const StartForm = ({ categories }) => {
+  const [difficulty, setDifficulty] = useState("easy");
+  const [category, setCategory] = useState();
+
+  const navigate = useNavigate();
+
+  const formActionHandler = (e) => {
+    e.preventDefault();
+    navigate(`/quiz/${difficulty}/${category}`);
+  };
+
+  return (
+    <div className="start-container">
+      {" "}
+      <h1>Quizzical</h1>
+      <p>Answer fun trivia questions! </p>
+      <form className="quiz-form" onSubmit={formActionHandler}>
+        <label htmlFor="select-category">
+          Category
+          <select
+            required
+            id="select-category"
+            onChange={(e) => setCategory(e.target.value)}
+            onBlur={(e) => setCategory(e.target.value)}
+          >
+            <option
+              value=""
+              style={{ display: "none" }}
+              default
+              selected
+              disabled
+            >
+              Please select a category
+            </option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="select-difficulty">
+          Difficulty
+          <select
+            id="select-difficulty"
+            onChange={(e) => setDifficulty(e.target.value)}
+            onBlur={(e) => setDifficulty(e.target.value)}
+          >
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </label>
+
+        <button className="btn start-btn">Start quiz</button>
+      </form>
+    </div>
+  );
+};
+
+export default StartForm;
