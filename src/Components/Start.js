03f8b377559645e0fde627d83590179ea14dab63
alicalimli <alicalimli76@gmail.com>
2022-06-14 08:05:02 +0300

    bug fix

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -4,7 +4,6 @@ const Start = () => {
   const navigate = useNavigate();
 
   const formActionHandler = (e) => {
-    console.log("ss");
     e.preventDefault();
     navigate("/quiz");
   };
@@ -13,10 +12,9 @@ const Start = () => {
     <div className="start-container">
       <h1>Quizzical</h1>
       <p>Answer fun trivia questions! </p>
-      <form onSubmit={formActionHandler} onBlur={formActionHandler}>
-        <label htmlFor="difficulty">Difficulty</label>
-        <select id="difficulty">
-          <option />
+      <form className="quiz-form" onSubmit={formActionHandler}>
+        <label htmlFor="select-difficulty">Difficulty</label>
+        <select id="select-difficulty">
           <option value="easy">Easy</option>
           <option value="medium">Medium</option>
           <option value="hard">Hard</option>
