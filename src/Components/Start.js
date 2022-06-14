58eaf4fad0f72d50ebb6fe4c28b4a188329865c4
alicalimli <alicalimli76@gmail.com>
2022-06-14 09:49:09 +0300

    conditional rendering adjustments

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 const Start = () => {
   const navigate = useNavigate();
   const [difficulty, setDifficulty] = useState("easy");
+  const [pending, setIsPending] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
   const [categories, setCategories] = useState([]);
   const [category, setCategory] = useState();
 
@@ -20,10 +22,8 @@ const Start = () => {
     try {
       const categories = await fetch("https://opentdb.com/api_category.php");
       const categoriesResults = await categories.json();
-      const categorySelection = document.querySelector("#select-category");
       setCategories(categoriesResults.trivia_categories);
-
-      return setCategory(categorySelection.firstChild.value);
+      return;
     } catch (error) {
       console.error(error);
     }
@@ -31,41 +31,56 @@ const Start = () => {
 
   return (
     <div className="start-container">
-      <h1>Quizzical</h1>
-      <p>Answer fun trivia questions! </p>
-      <form className="quiz-form" onSubmit={formActionHandler}>
-        <label htmlFor="select-category">
-          Category
-          <select
-            required
-            id="select-category"
-            onChange={(e) => setCategory(e.target.value)}
-            onBlur={(e) => setCategory(e.target.value)}
-          >
-            {categories.length !== 0
-              ? categories.map((category) => (
+      {console.log(categories.length)}
+      {categories.length !== 0 ? (
+        <div>
+          {" "}
+          <h1>Quizzical</h1>
+          <p>Answer fun trivia questions! </p>
+          <form className="quiz-form" onSubmit={formActionHandler}>
+            <label htmlFor="select-category">
+              Category
+              <select
+                required
+                id="select-category"
+                onChange={(e) => setCategory(e.target.value)}
+                onBlur={(e) => setCategory(e.target.value)}
+              >
+                <option
+                  value=""
+                  style={{ display: "none" }}
+                  default
+                  selected
+                  disabled
+                >
+                  Please select a category
+                </option>
+                {categories.map((category) => (
                   <option key={category.id} value={category.id}>
                     {category.name}
                   </option>
-                ))
-              : ""}
-          </select>
-        </label>
-        <label htmlFor="select-difficulty">
-          Difficulty
-          <select
-            id="select-difficulty"
-            onChange={(e) => setDifficulty(e.target.value)}
-            onBlur={(e) => setDifficulty(e.target.value)}
-          >
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
-          </select>
-        </label>
+                ))}
+              </select>
+            </label>
+            <label htmlFor="select-difficulty">
+              Difficulty
+              <select
+                id="select-difficulty"
+                onChange={(e) => setDifficulty(e.target.value)}
+                onBlur={(e) => setDifficulty(e.target.value)}
+              >
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+            </label>
 
-        <button className="btn start-btn">Start quiz</button>
-      </form>
+            <button className="btn start-btn">Start quiz</button>
+          </form>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
