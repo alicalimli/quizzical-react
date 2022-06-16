c576da56eeced938d7c94a0a51982034a385c34f
alicalimli <alicalimli76@gmail.com>
2022-06-16 07:50:04 +0300

    style fixes

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Modal.css";
 
 const Modal = ({ children }) => {
-  return <div className="modal">{children}</div>;
+  return <div className="modal"> {children} </div>;
 };
 
 export default Modal;
