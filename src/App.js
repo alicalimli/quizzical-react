58584360569ef3ea2dc6b1d33983e3dc89a40c4a
alicalimli <alicalimli76@gmail.com>
2022-06-18 07:56:26 +0300

    updates in App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Start from "./Components/StartPage/Start";
 import Quiz from "./Components/QuizPage/Quiz";
 import Error from "./Components/Error/Error";
+import { FaMoon } from "react-icons/fa";
 import { StrictMode } from "react";
 
 const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
-        <Routes>
-          <Route
-            path="/quiz/:difficulty/:categoryName/:category"
-            element={<Quiz />}
-          />
-          <Route path="/" element={<Start />} />
-          <Route path="*" element={<Error error404="true" />} />
-        </Routes>
+        <div className="App">
+          <header>
+            <button>
+              <FaMoon />
+            </button>
+          </header>
+          <div className="content">
+            <Routes>
+              <Route
+                path="/quiz/:difficulty/:categoryName/:category"
+                element={<Quiz />}
+              />
+              <Route path="/" element={<Start />} />
+              <Route path="*" element={<Error error404="true" />} />
+            </Routes>
+          </div>
+        </div>
       </BrowserRouter>
     </StrictMode>
   );
