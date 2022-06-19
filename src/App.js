394b0dd2b109d749320595515387a34e516592c3
alicalimli <alicalimli76@gmail.com>
2022-06-19 10:57:21 +0300

    created quizInfoContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 
-import React, { useEffect, StrictMode, createContext } from "react";
+import React, { useEffect, useState, StrictMode, createContext } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FaMoon } from "react-icons/fa";
@@ -13,8 +13,17 @@ import Start from "./Pages/StartPage/Start";
 import Quiz from "./Pages/QuizPage/Quiz";
 import Error from "./Pages/Error/Error";
 
+export const quizInfoContext = createContext({});
+
 const App = () => {
   const { data: darkMode, setData: setDarkMode } = useGetLocalData("darkmode");
+
+  const quizInfoState = useState({
+    name: "",
+    category: "",
+    difficulty: "",
+    categoryName: "",
+  });
 
   const toggleDarkMode = () => {
     const body = document.body;
@@ -38,27 +47,29 @@ const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
-        <div className="App">
-          <button
-            onClick={() => {
-              setDarkMode(!darkMode);
-            }}
-            className="darkmode-btn"
-          >
-            <FaMoon className="moon-icon" />
-            <BsFillSunFill className="sun-icon" />
-          </button>
-          <div className="content">
-            <Routes>
-              <Route
-                path="/quiz/:difficulty/:categoryName/:category"
-                element={<Quiz />}
-              />
-              <Route path="/" element={<Start />} />
-              <Route path="*" element={<Error error404="true" />} />
-            </Routes>
+        <quizInfoContext.Provider value={quizInfoState}>
+          <div className="App">
+            <button
+              onClick={() => {
+                setDarkMode(!darkMode);
+              }}
+              className="darkmode-btn"
+            >
+              <FaMoon className="moon-icon" />
+              <BsFillSunFill className="sun-icon" />
+            </button>
+            <div className="content">
+              <Routes>
+                <Route
+                  path="/quiz/:difficulty/:categoryName/:category"
+                  element={<Quiz />}
+                />
+                <Route path="/" element={<Start />} />
+                <Route path="*" element={<Error error404="true" />} />
+              </Routes>
+            </div>
           </div>
-        </div>
+        </quizInfoContext.Provider>
       </BrowserRouter>
     </StrictMode>
   );
