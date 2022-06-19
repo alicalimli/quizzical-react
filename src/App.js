8e9a344d03b7dd2a0ca128abb276edc785536c42
alicalimli <alicalimli76@gmail.com>
2022-06-19 10:31:17 +0300

    import adjustments in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+
+import React, { useEffect, StrictMode, createContext } from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Start from "./Components/StartPage/Start";
-import Quiz from "./Components/QuizPage/Quiz";
-import Error from "./Components/Error/Error";
 import { FaMoon } from "react-icons/fa";
 import { BsFillSunFill } from "react-icons/bs";
 
-import { StrictMode } from "react";
 import useLocalStorage from "./Hooks/useLocalStorage";
 import useGetLocalData from "./Hooks/useGetLocalData";
+
+import Start from "./Pages/StartPage/Start";
+import Quiz from "./Pages/QuizPage/Quiz";
+import Error from "./Pages/Error/Error";
 
 const App = () => {
   const { data: darkMode, setData: setDarkMode } = useGetLocalData("darkmode");
