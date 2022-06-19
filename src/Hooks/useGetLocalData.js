5f624c855af6e63586e87be3a0c0b85dd9847248
alicalimli <alicalimli76@gmail.com>
2022-06-19 11:40:58 +0300

    refactor useGetLocalData

--- src/Hooks/useGetLocalData.js
+++ src/Hooks/useGetLocalData.js
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 
 const useGetLocalData = (key) => {
   const [data, setData] = useState();
-  useEffect(() => {
-    const localData = JSON.parse(localStorage.getItem(key));
 
-    return setData(localData);
+  useEffect(() => {
+    const localData = JSON.parse(localStorage.getItem("quizzicalData"));
+
+    return setData(localData[key]);
   }, [key]);
 
   return { data, setData };
